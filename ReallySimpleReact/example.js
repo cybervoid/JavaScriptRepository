c6cc8fa6a00d2045ddba6b17ba42c5171a9897e0
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-09-23 10:20:45 -0400

    more basic react

--- ReallySimpleReact/example.js
+++ ReallySimpleReact/example.js
@@ -1,2 +1,16 @@
 //babel pre-processes the external javascript file.
-alert("sttest");
+
+//create a dom element with react
+class AppComponent extends React.Component{
+  render(){
+    return React.createElement("h1",
+      {className: "header"},
+      "Hello world"
+    );
+  }
+}
+
+ReactDOM.render(
+  React.createElement(AppComponent, null),  //render this
+  document.getElementById("application") //output it here
+);
