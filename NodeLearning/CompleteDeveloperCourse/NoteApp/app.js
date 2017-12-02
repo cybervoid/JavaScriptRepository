f19140e55baf22799b36ff0eeb48e73c7be74d0b
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-02 08:31:51 -0500

    app.js file filesystem append code added

--- NodeLearning/CompleteDeveloperCourse/NoteApp/app.js
+++ NodeLearning/CompleteDeveloperCourse/NoteApp/app.js
@@ -1,1 +1,6 @@
-console.log("Starting app");
+console.log("Starting Note app");
+
+const fs = require('fs'); //pass in the name of the module we'd like to require
+
+fs.appendFile('greetings.txt', 'Hello from text file');
+//node app.js   -> running this should create a file with contents as the second argument
