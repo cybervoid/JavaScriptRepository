c2c5b94a86fd2ec4239fca41f5bf52934c6f1e3a
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-02 07:21:19 -0500

    implemented the simplest node server possible

--- NodeLearning/HelloWorld/app.js
+++ NodeLearning/HelloWorld/app.js
@@ -2,5 +2,5 @@ var http = require("http");
 
 //run on localhost, listen on port 3000
 http.createServer(function(request, response){
-
+  response.end("hello world!"); //call end whenever client is requesting from a get request
 });
