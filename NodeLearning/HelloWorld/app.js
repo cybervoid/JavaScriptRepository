b1481b01fae5aeb6b52b7ce2daf68e630756250b
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-02 07:41:08 -0500

    typos/bug fixes

--- NodeLearning/HelloWorld/app.js
+++ NodeLearning/HelloWorld/app.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 //run on localhost, listen on port 3000
 http.createServer(function(request, response){
-  res.writeHead(200, {'Content-type' : 'text/plan' })
+  response.writeHead(200, {'Content-type' : 'text/plain' })
   response.end("hello world!"); //call end whenever client is requesting from a get request
 }).listen(3000);
 
