522381289fe9da5380544b140367664161cf96d4
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-02 08:34:55 -0500

    callback function code added

--- NodeLearning/CompleteDeveloperCourse/NoteApp/app.js
+++ NodeLearning/CompleteDeveloperCourse/NoteApp/app.js
@@ -2,5 +2,18 @@ console.log("Starting Note app");
 
 const fs = require('fs'); //pass in the name of the module we'd like to require
 
-fs.appendFile('greetings.txt', 'Hello from text file');
-//node app.js   -> running this should create a file with contents as the second argument
+useCallback = false;
+
+if(useCallback == true)
+{
+  //run this as a callback function
+  fs.appendFile("greetingsAsCallback.txt", "Hello from callback function!", function(err){
+    if(err){
+      console.log("Unable to write to file");
+    }
+  });
+}
+else {
+  //run file create without a callback function
+  fs.appendFile('greetings.txt', 'Hello from text file');
+}//node app.js   -> running this should create a file with contents as the second argument
