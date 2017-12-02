c55365cf66cdc072e1183eb653af94cc87d4dab7
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-02 07:37:33 -0500

    Added missing listen method extension with port

---
+++ NodeLearning/HelloWorld/README.md
@@ -0,0 +1,7 @@
+<h2>HelloWorld</h2>
+<p>How to run the node server
+<ul>
+<li>Open a terminal and navigate to the HelloWorld directory</li>
+<li>type this command in the terminal: <b>node app.js</b></li>
+<li>Navigate to this url in a browser</li>
+</ul>
--- NodeLearning/HelloWorld/app.js
+++ NodeLearning/HelloWorld/app.js
@@ -2,5 +2,10 @@ var http = require("http");
 
 //run on localhost, listen on port 3000
 http.createServer(function(request, response){
+  res.writeHead(200, {'Content-type' : 'text/plan' })
   response.end("hello world!"); //call end whenever client is requesting from a get request
-});
+}).listen(3000);
+
+console.log("server is running on 127.0.0.1:3000");
+//to run the file, run this in a terminal (in this directory!)
+//node app.js
